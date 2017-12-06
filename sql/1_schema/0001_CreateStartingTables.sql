1f28116e831741f543ab6b34d588039e864e91be
James Stormes <james.s@digitalroominc.com>
2017-12-06 10:23:21 -0600

    Testing database updates.

--- sql/1_schema/0001_CreateStartingTables.sql
+++ sql/1_schema/0001_CreateStartingTables.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.5
+-- version 4.7.6
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Dec 01, 2017 at 05:06 PM
--- Server version: 10.2.10-MariaDB-10.2.10+maria~jessie
+-- Generation Time: Dec 06, 2017 at 04:21 PM
+-- Server version: 10.2.11-MariaDB-10.2.11+maria~jessie
 -- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -24,10 +24,12 @@ USE `ezoauth2`;
 -- Table structure for table `client`
 --
 
-CREATE TABLE `client` (
-  `client_id` bigint(20) NOT NULL,
-  `client_name` varchar(50) COLLATE utf8mb4_bin NOT NULL COMMENT 'Name of client applicaiton'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+CREATE TABLE IF NOT EXISTS `client` (
+  `client_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `client_name` varchar(50) COLLATE utf8mb4_bin NOT NULL COMMENT 'Name of client applicaiton',
+  PRIMARY KEY (`client_id`),
+  UNIQUE KEY `client_name` (`client_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `client`
@@ -39,21 +41,23 @@ INSERT IGNORE INTO `client` (`client_id`, `client_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `phone`
+-- Table structure for table `contact_method`
 --
 
-CREATE TABLE `phone` (
-  `phone_id` bigint(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS `contact_method` (
+  `contact_method_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) NOT NULL,
-  `type` enum('unknown','landline','mobile','') COLLATE utf8mb4_bin NOT NULL DEFAULT 'unknown',
-  `phone_number` varchar(50) COLLATE utf8mb4_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `type` enum('unknown','landline','mobile','skype') COLLATE utf8mb4_bin NOT NULL DEFAULT 'unknown',
+  `contact_value` varchar(150) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`contact_method_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
--- Dumping data for table `phone`
+-- Dumping data for table `contact_method`
 --
 
-INSERT IGNORE INTO `phone` (`phone_id`, `user_id`, `type`, `phone_number`) VALUES
+INSERT IGNORE INTO `contact_method` (`contact_method_id`, `user_id`, `type`, `contact_value`) VALUES
 (1, 2, 'mobile', '817-676-4291');
 
 -- --------------------------------------------------------
@@ -62,24 +66,26 @@ INSERT IGNORE INTO `phone` (`phone_id`, `user_id`, `type`, `phone_number`) VALUE
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `user_id` bigint(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(50) COLLATE utf8mb4_bin NOT NULL COMMENT 'Email Address',
   `password` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'Password Hash',
   `full_name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'Formal Name',
-  `name_addressed_by` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'Informal (First Name)'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='OAuth2 User Profile Data';
+  `name_addressed_by` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'Informal (First Name)',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='OAuth2 User Profile Data';
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT IGNORE INTO `user` (`user_id`, `user_name`, `password`, `full_name`, `name_addressed_by`) VALUES
-(2, 'jstormes@stormes.net', 'Xtx!41365', 'James W Stormes', 'Jim2'),
+(2, 'jstormes@stormes.net', '', 'James W Stormes', 'ãƒ«ãƒ“ ãƒ³ãƒ„ã‚¢ ã‚¦ã‚§ãƒ–ã‚¢ ãµã¹ã‹ã‚‰ãš ã‚»ã‚·ãƒ“ãƒªãƒ†'),
 (3, 'jstormes@yahoo.com', '', 'James Stormes', 'James'),
 (4, 'tom@tome.com', 'password', 'Tom Man', 'Tom'),
-(6, 'test@test.com', 'password', 'test user', 'test'),
-(7, 'test2@test.com', 'Xtx!41365', 'test', 'test'),
+(6, 'test@test.com', '', 'ã‚¤ãƒ‰ãƒ©ã‚¤ãƒ³ ã‚ªãƒ–ã‚¸ã‚§ã‚¯', 'test'),
+(7, 'test2@test.com', '', 'Ð›Ð¾Ñ€ÐµÐ¼ Ð¸Ð¿ÑÑƒÐ¼', 'test'),
 (20, 'test3@test.com', 'password', 'test', 'test');
 
 -- --------------------------------------------------------
@@ -88,9 +94,11 @@ INSERT IGNORE INTO `user` (`user_id`, `user_name`, `password`, `full_name`, `nam
 -- Table structure for table `user_to_client`
 --
 
-CREATE TABLE `user_to_client` (
+CREATE TABLE IF NOT EXISTS `user_to_client` (
   `user_id` bigint(20) NOT NULL,
-  `client_id` bigint(20) NOT NULL
+  `client_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`client_id`),
+  KEY `client` (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
@@ -98,64 +106,18 @@ CREATE TABLE `user_to_client` (
 --
 
 --
--- Indexes for table `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`client_id`),
-  ADD UNIQUE KEY `client_name` (`client_name`);
-
---
--- Indexes for table `phone`
---
-ALTER TABLE `phone`
-  ADD PRIMARY KEY (`phone_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
 -- Indexes for table `user`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `user_name` (`user_name`);
 ALTER TABLE `user` ADD FULLTEXT KEY `user_fulltext` (`full_name`,`name_addressed_by`,`user_name`);
-
---
--- Indexes for table `user_to_client`
---
-ALTER TABLE `user_to_client`
-  ADD PRIMARY KEY (`user_id`,`client_id`),
-  ADD KEY `client` (`client_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `client`
---
-ALTER TABLE `client`
-  MODIFY `client_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `phone`
---
-ALTER TABLE `phone`
-  MODIFY `phone_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `phone`
+-- Constraints for table `contact_method`
 --
-ALTER TABLE `phone`
+ALTER TABLE `contact_method`
   ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
@@ -165,6 +127,7 @@ ALTER TABLE `user_to_client`
   ADD CONSTRAINT `client` FOREIGN KEY (`client_id`) REFERENCES `client` (`client_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 COMMIT;
+
 
 CREATE USER 'ezoauth2'@'%' IDENTIFIED VIA mysql_native_password USING 'oauthuser';
 GRANT USAGE ON *.* TO 'ezoauth2'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
